5fb7a9a21bc181bc54327d965776f97088ba4d88
antsuarz <uo270543@uniovi.es>
2024-09-18 17:08:00 +0200

    colores en la consola

--- piloto2.py
+++ piloto2.py
@@ -6,16 +6,16 @@ def detectar_emociones(imagen_path):
     img = cv2.imread(imagen_path)
     try:
         
-        resultado = DeepFace.analyze(img, actions=['emotion'])
-        emocion_dominante = resultado[0]['dominant_emotion']
-        emociones = resultado[0]['emotion'] 
+        result = DeepFace.analyze(img, actions=['emotion'])
+        dominant_emotion = result[0]['dominant_emotion']
+        emotion = result[0]['emotion'] 
         
-        print("-----------------------------------------------")
-        for emocion, porcentaje in emociones.items():
-            print(f"{emocion}: {porcentaje:.2f}%")
-        print("-----------------------------------------------")
+        print("\033[93m" + "-----------------------------------------------" + "\033[0m")
+        for emotion, porcentaje in emotion.items():
+            print(f"{emotion}: {porcentaje:.2f}%")
+        print("\033[93m" + "-----------------------------------------------" + "\033[0m")
         
-        print(f"La emoción detectada es {emocion_dominante}.")
+        print("\033[92m" + f"Dominant emotion: {dominant_emotion}." + "\033[0m")
     
     except Exception as e:
-        print(f"Ha ocurrido un error al analizar la imagen: {e}")
+        print("\033[91m" + f"ERROR: {e}" + "\033[0m")
